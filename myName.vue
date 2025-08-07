d573d3257d0c3957029706476312e57590e3fb8f
mkan0048-code <mkan0048@student.monash.edu>
2025-08-08 06:19:06 +1000

    Add files via upload

---
+++ myName.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="my-name">
+    <h2>Welcome, I'm Madhav Kanakhara!</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyName',
+};
+</script>
+
+<style scoped>
+.my-name {
+  font-size: 24px;
+  color: #3eaf7c;
+  margin-top: 20px;
+}
+</style>
